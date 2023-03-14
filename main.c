8e79971a39c5e2a3f7f36c832a38715aee277c6c
Hajar Ouaslam <houaslam@e2r9p5.1337.ma>
2023-03-14 22:52:18 +0100

    minishell

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: houaslam <houaslam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 15:34:20 by houaslam          #+#    #+#             */
-/*   Updated: 2023/03/14 22:37:56 by houaslam         ###   ########.fr       */
+/*   Updated: 2023/03/14 22:52:10 by houaslam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	creat_env(char **en, t_env *env)
 	while (en[i])
 	{
 		j = -1;
-		tmp->ptr = malloc(sizeof(char) * strlen(en[i]));
+		tmp->ptr = malloc(sizeof(en[i]));
 		while (en[i][++j])
 		{
 			tmp->ptr[j] = en[i][j];
@@ -38,14 +38,17 @@ void	creat_env(char **en, t_env *env)
 
 int	main(int ac, char **av, char **en)
 {
+	(void)av;
 	t_env	*env;
 
 	if (ac == 1)
 	{
+		env = NULL;
 		creat_env(en, env);
 		while (env)
 		{
 			printf("%s\n", env->ptr);
+			printf("%s\n", env->next->ptr);
 			env = env->next;
 		}
 		// minishell(ac, av, env);
